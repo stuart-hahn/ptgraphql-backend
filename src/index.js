997a756c73e288bc4c635c56de1e05aedb8676bd
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-08-18 18:08:31 -0400

    add 'feed' query that returns a list of all tournaments stored in-memory (no db yet)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
 const { GraphQLServer } = require('graphql-yoga')
 
+let tournaments = [
+  {
+    id: 'fnf-0',
+    name: "Friday Night Football #1",
+    url: 'twitch.tv/mutheadtv'
+  },
+  {
+    id: 'fnf-1',
+    name: "Friday Night Football #2",
+    url: 'twitch.tv/mutheadtv'
+  },
+]
+
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Tournament!]!
+}
+
+type Tournament {
+  id: ID!
+  name: String!
+  url: String!
 }
 `
 
 const resolvers = {
   Query: {
-    info: () => 'This is the API of a player tracker'
+    info: () => 'This is the API of a player tracker',
+    feed: () => tournaments
+  },
+  Tournament: {
+    id: (parent) => parent.id,
+    name: (parent) => parent.name,
+    url: (parent) => parent.url
   }
 }
 

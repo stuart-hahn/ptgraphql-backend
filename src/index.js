4792063062f25f36b0842613257df723287d55e2
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-08-18 21:05:57 -0400

    add query for individual tournament by using prisma's findOne() method

--- src/index.js
+++ src/index.js
@@ -1,70 +1,59 @@
+const { PrismaClient } = require('@prisma/client')
 const { GraphQLServer } = require('graphql-yoga')
-
-let tournaments = [
-  {
-    id: 'fnf-0',
-    name: "Friday Night Football #1",
-    url: 'twitch.tv/mutheadtv'
-  },
-  {
-    id: 'fnf-1',
-    name: "Friday Night Football #2",
-    url: 'twitch.tv/mutheadtv'
-  },
-]
-
-let idCount = tournaments.length
 
 const resolvers = {
   Query: {
     info: () => 'This is the API of a player tracker',
-    feed: () => tournaments,
-    tournament: (parent, args) => {
-      const tournament = tournaments.find(t => t.id === args.id)
-      return tournament
+    feed: async (parent, args, context) => {
+      return context.prisma.tournament.findMany()
+    },
+    tournament: async (parent, args, context) => {
+      return context.prisma.tournament.findOne({
+        where: { id: Number(args.id) }
+      })
     }
   },
   Mutation: {
-    createTournament: (parent, args) => {
-      const tournament = {
-        id: `fnf-${idCount++}`,
-        name: args.name,
-        url: args.url
-      }
-      tournaments.push(tournament)
-      return tournament
+    createTournament: (parent, args, { prisma }) => {
+      const newTournament = prisma.tournament.create({
+        data: {
+          name: args.name,
+          url: args.url
+        }
+      })
+      return newTournament
     },
-    updateTournament: (parent, args) => {
-      const tournament = tournaments.find(t => t.id === args.id)
-      if (!tournament) {
-        throw new Error("Tournament not found")
-      }
-      if (args.name) {
-        tournament.name = args.name
-      }
-      if (args.url) {
-        tournament.url = args.url
-      }
-      return tournament
-    },
-    deleteTournament: (parent, args) => {
-      const tournament = tournaments.find(t => t.id === args.id)
-      if (!tournament) {
-        throw new Error('Could not delete tournament')
-      }
-      tournaments = tournaments.filter(t => t.id !== tournament.id)
-      return tournament
-    }
+    // updateTournament: (parent, args, { prisma }) => {
+    //   const tournament = tournaments.find(t => t.id === args.id)
+    //   if (!tournament) {
+    //     throw new Error("Tournament not found")
+    //   }
+    //   if (args.name) {
+    //     tournament.name = args.name
+    //   }
+    //   if (args.url) {
+    //     tournament.url = args.url
+    //   }
+    //   return tournament
+    // },
+    // deleteTournament: (parent, args, { prisma }) => {
+    //   const tournament = tournaments.find(t => t.id === args.id)
+    //   if (!tournament) {
+    //     throw new Error('Could not delete tournament')
+    //   }
+    //   tournaments = tournaments.filter(t => t.id !== tournament.id)
+    //   return tournament
+    // }
   }
-  // Tournament: {
-  //   id: (parent) => parent.id,
-  //   name: (parent) => parent.name,
-  //   url: (parent) => parent.url
-  // }
 }
+
+const prisma = new PrismaClient()
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  context: {
+    prisma
+  }
 })
 server.start(() => console.log('Server is up on http://localhost:4000'))
